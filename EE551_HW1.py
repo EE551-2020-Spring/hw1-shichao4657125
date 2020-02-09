a48d6f77356ec589577074587434b9261d9d7987
shichao4657125 <54757300+shichao4657125@users.noreply.github.com>
2020-02-09 17:24:23 -0500

    Update EE551_HW1.py

--- EE551_HW1.py
+++ EE551_HW1.py
@@ -6,6 +6,7 @@ Python Core object Types
 
 import math
 
+
 def numbers_and_strings():
     """
     This is to review numbers and strings and basic operations.
@@ -15,13 +16,23 @@ def numbers_and_strings():
 
     # Assign a string "Stevens" to a variable y
 
+    y = "Stevens"
+
     # Repeat variable y 5 times
+
+    z = y * 5
 
     # What is the length of z?
 
+    length = len(z)
+
     # Concatenate variable y with string " is good"
 
+    m = y + " is good"
+
     # Replace "good" with "awesome" in variable m and assign it to a new variable n
+
+    n = m.replace("good", "awesome")
 
     return x, y, z, length, m, n
 
@@ -34,6 +45,8 @@ def lists():
 
     # Split variable n on a delimiter space into a list of substrings
 
+    p = n.split(" ", 2)
+
     # Get all the items past the first of the third substring
 
     # Create a 3 x 3 matrix as nested list such that
@@ -41,16 +54,33 @@ def lists():
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
 
+    r = [[1, 4, 5],
+         [6, 10, 11],
+         [12, 17, 38]]
+
     # Collect the items in the last column of matrix A using list comprehension
 
+    c = []
+
+    for row in r:
+        c.append(row[2])
+
     # Collect only the even items of the diagonal of matrix A using list comprehension
+
+    d = []
+    for i in range(len(r)):
+        if r[i][i] % 2 == 0:
+            d.append(r[i][i])
 
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
 
-    return p, r, c, d, o
+    o = []
+    for c in "Stevens":
+        o.append(ord(c))
 
+    return p, r, c, d, o
 
 def dictionaries():
     """
@@ -61,29 +91,45 @@ def dictionaries():
     #   quantity => 4
     #   color => "green"
 
+    f = {"fruit":"apple", "quantity":"4", "color":"green"}
+
     # Get the item in dictionary f that the key "fruit" maps to
+    a = f["fruit"]
 
     # Increase the quantity of f by 1
     # IMPLEMENT IT HERE
+    f["quantity"]=5
 
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
 
+    p = {"name":{"first_name":"Grace","last_name":"Hopper"},"jobs":["scientist","engineer"],"age":"85"}
+
     # Add "programmer" to the list of jobs Grace has
     # IMPLEMENT IT HERE
 
+    p["jobs"].append("programmer")
+
     # Get the third job Grace has that you recently added
 
+    p["jobs"][2]
+
     # Use the sort() function to get sorted keys of amazing_grace in alphabetically ascending order
+
+
+    k = []
+    for c in "amazing_grace":
+        k.append(ord(c))
+
+    k.sort()
 
     return a, f, p, k
 
 numbers_and_strings()
 lists()
 dictionaries()
-
 
 
 
